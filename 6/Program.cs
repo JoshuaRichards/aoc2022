f4f4f7920ade1cbb57d1424ced75abe19e423830
Joshua Richards <8954297+JoshuaRichards@users.noreply.github.com>
2022-12-06 16:15:23 +1100

    day 6

---
+++ 6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day6;
+
+public class Program
+{
+    public static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        Console.WriteLine(FindMarker(input, 4));
+        Console.WriteLine(FindMarker(input, 14));
+    }
+
+    public static int FindMarker(string input, int seqLength)
+    {
+        return Enumerable.Range(0, input.Length)
+            .First(i => input[i..(i + seqLength)].Distinct().Count() == seqLength)
+            + seqLength;
+    }
+}
