adcffa2ed0292ea45e91928d2f10c0b2628a2511
Joshua Richards <8954297+JoshuaRichards@users.noreply.github.com>
2022-12-13 12:57:36 +1100

    clean up day 12

--- 12/Program.cs
+++ 12/Program.cs
@@ -5,10 +5,19 @@ public class Program
     public static void Main()
     {
         var grid = ReadGrid();
-        var part1 = Dijstra(grid, grid.First(kvp => kvp.Value == 'S').Key);
+        var part1 = Dijstra(
+            grid,
+            grid.First(kvp => kvp.Value == 'S').Key,
+            grid.Where(kvp => kvp.Value == 'E').Select(kvp => kvp.Key).ToArray(),
+            (source, dest) => dest <= source + 1
+        );
         Console.WriteLine(part1);
 
-        var part2 = ReverseDijstra(grid, grid.First(kvp => kvp.Value == 'E').Key, grid.Where(kvp => kvp.Value == 'S' || kvp.Value == 'a').Select(kvp => kvp.Key).ToArray());
+        var part2 = Dijstra(
+            grid, grid.First(kvp => kvp.Value == 'E').Key,
+            grid.Where(kvp => kvp.Value == 'S' || kvp.Value == 'a').Select(kvp => kvp.Key).ToArray(),
+            (source, dest) => source <= dest + 1
+        );
         Console.WriteLine(part2);
     }
 
@@ -29,101 +38,32 @@ public class Program
         return ret;
     }
 
-    public static IEnumerable<(int x, int y)> GetNeighbours(Dictionary<(int x, int y), char> grid, int x, int y)
+    public static IEnumerable<(int x, int y)> GetNeighbours(Dictionary<(int x, int y), char> grid, (int x, int y) source, Func<char, char, bool> isEdge)
     {
         return new (int x, int y)[] { (-1, 0), (1, 0), (0, -1), (0, 1) }
-            .Select(dir => (x + dir.x, y + dir.y))
+            .Select(dir => (source.x + dir.x, source.y + dir.y))
             .Where(point => grid.ContainsKey(point))
             .Where(point =>
             {
-                var sourceValue = grid[(x, y)];
+                var sourceValue = grid[source];
                 var destValue = grid[point];
                 if (sourceValue == 'S') sourceValue = 'a';
                 if (sourceValue == 'E') sourceValue = 'z';
                 if (destValue == 'S') destValue = 'a';
                 if (destValue == 'E') destValue = 'z';
 
-                return destValue <= sourceValue + 1;
+                return isEdge(sourceValue, destValue);
             });
     }
 
-    public static IEnumerable<(int x, int y)> GetReverseNeighbours(Dictionary<(int x, int y), char> grid, int x, int y)
+    public static double Dijstra(Dictionary<(int x, int y), char> graph, (int x, int y) source, (int x, int y)[] targets, Func<char, char, bool> isEdge)
     {
-        return new (int x, int y)[] { (-1, 0), (1, 0), (0, -1), (0, 1) }
-            .Select(dir => (x + dir.x, y + dir.y))
-            .Where(point => grid.ContainsKey(point))
-            .Where(point =>
-            {
-                var sourceValue = grid[(x, y)];
-                var destValue = grid[point];
-                if (sourceValue == 'S') sourceValue = 'a';
-                if (sourceValue == 'E') sourceValue = 'z';
-                if (destValue == 'S') destValue = 'a';
-                if (destValue == 'E') destValue = 'z';
-
-                return sourceValue <= destValue + 1;
-            });
-    }
-
-    public static int Dijstra(Dictionary<(int x, int y), char> graph, (int x, int y) source)
-    {
-        Console.WriteLine(source);
-        var dist = new Dictionary<(int x, int y), int>();
+        var dist = new Dictionary<(int x, int y), double>();
         var prev = new Dictionary<(int x, int y), (int x, int y)>();
         var unvisited = new HashSet<(int x, int y)>(graph.Keys);
         foreach (var point in graph.Keys)
         {
-            dist[point] = int.MaxValue;
-        }
-        dist[source] = 0;
-
-        var target = graph.First(kvp => kvp.Value == 'E').Key;
-        while (unvisited.Any())
-        {
-            var u = unvisited.MinBy(point => dist[point]);
-            unvisited.Remove(u);
-
-            var neighbours = GetNeighbours(graph, u.x, u.y)
-                .Where(n => unvisited.Contains(n));
-            foreach (var neighbour in neighbours)
-            {
-                var alt = dist[u] + 1;
-                if (alt < dist[neighbour])
-                {
-                    dist[neighbour] = alt;
-                    prev[neighbour] = u;
-                }
-            }
-
-            if (u == target) break;
-        }
-
-        var current = target;
-        var count = 0;
-        while (current != source)
-        {
-            count++;
-            if (prev.TryGetValue(current, out var next))
-            {
-                current = next;
-            }
-            else
-            {
-                return int.MaxValue;
-            }
-        }
-
-        return count;
-    }
-
-    public static int ReverseDijstra(Dictionary<(int x, int y), char> graph, (int x, int y) source, (int x, int y)[] targets)
-    {
-        var dist = new Dictionary<(int x, int y), int>();
-        var prev = new Dictionary<(int x, int y), (int x, int y)>();
-        var unvisited = new HashSet<(int x, int y)>(graph.Keys);
-        foreach (var point in graph.Keys)
-        {
-            dist[point] = int.MaxValue;
+            dist[point] = double.PositiveInfinity;
         }
         dist[source] = 0;
 
@@ -132,7 +72,7 @@ public class Program
             var u = unvisited.MinBy(point => dist[point]);
             unvisited.Remove(u);
 
-            var neighbours = GetReverseNeighbours(graph, u.x, u.y)
+            var neighbours = GetNeighbours(graph, u, isEdge)
                 .Where(n => unvisited.Contains(n));
             foreach (var neighbour in neighbours)
             {
@@ -145,25 +85,6 @@ public class Program
             }
         }
 
-        return targets.Min(target =>
-        {
-            var current = target;
-            var count = 0;
-            while (current != source)
-            {
-                count++;
-                if (prev.TryGetValue(current, out var next))
-                {
-                    current = next;
-                }
-                else
-                {
-                    return int.MaxValue;
-                }
-            }
-
-            return count;
-        });
-
+        return targets.Min(target => dist[target]);
     }
 }
