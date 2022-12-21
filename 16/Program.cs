5ce61d2046554adfbd643608bf0df694edd150ee
Joshua Richards <8954297+JoshuaRichards@users.noreply.github.com>
2022-12-21 23:53:32 +1100

    day 16 in progress

--- 16/Program.cs
+++ 16/Program.cs
@@ -8,8 +8,11 @@ public class Program
     {
         var graph = ReadValves().ToDictionary(v => v.Name);
 
-        var part1 = Search(graph, 30);
-        Console.WriteLine(part1);
+        // var part1 = Search(graph, 30);
+        // Console.WriteLine(part1);
+
+        var part2 = Search2(graph, 26);
+        Console.WriteLine(part2);
     }
 
     public static int GetMaxFlow(Dictionary<string, Valve> graph, Valve current, string[] visited, int timeRemaining)
@@ -29,7 +32,7 @@ public class Program
         var shortKings = ShortKings(graph);
         open.Enqueue(new State
         {
-            CurrentValve = "AA",
+            SelfValve = "AA",
             OpenValves = graph.Values.Where(v => v.FlowRate == 0).ToDictionary(v => v.Name, _ => 0),
             TimeRemaining = timeRemaining,
         });
@@ -47,7 +50,7 @@ public class Program
             var current = open.Dequeue();
             closed.Add(current);
 
-            Console.WriteLine($"at {current.CurrentValve}, timeRemaining: {current.TimeRemaining}, {open.Count()} states left");
+            Console.WriteLine($"at {current.SelfValve}, timeRemaining: {current.TimeRemaining}, {open.Count()} states left");
 
             var newBest = current.Score > best.GetValueOrDefault(current.TimeRemaining);
             if (newBest)
@@ -61,6 +64,44 @@ public class Program
         return best.Values.Max();
     }
 
+    public static int Search2(Dictionary<string, Valve> graph, int timeRemaining)
+    {
+        var open = new Queue<State>();
+        var shortKings = ShortKings(graph);
+        open.Enqueue(new State
+        {
+            SelfValve = "AA",
+            ElephantValve = "AA",
+            OpenValves = graph.Values.Where(v => v.FlowRate == 0).ToDictionary(v => v.Name, _ => 0),
+            TimeRemaining = timeRemaining,
+        });
+        // var closed = new HashSet<State>();
+
+        var best = open.First();
+
+        while (open.Any())
+        {
+            var current = open.Dequeue();
+            // closed.Add(current);
+            if (current.TimeRemaining < 0) continue;
+
+            Console.WriteLine($"{current.TimeRemaining} remaining, {open.Count()} states left, best: {best.Score}");
+
+            if (current.Score > best.Score) best = current;
+
+            var possibleMoves = GetPossibleMoves2(current, graph, shortKings);
+            foreach (var move in possibleMoves) open.Enqueue(move);
+        }
+
+        var path = new List<State>();
+        var c = best;
+        path.Add(c);
+        while (c.Parent is not null) path.Add(c = c.Parent);
+        path.Reverse();
+
+        return best.Score;
+    }
+
     public static State[] GetPossibleMoves(State state, Dictionary<string, Valve> graph, Dictionary<(string, string), int> shortKings)
     {
         if (state.TimeRemaining == 0)
@@ -69,21 +110,158 @@ public class Program
             return new State[0];
         var ret = new List<State>();
 
-        var destinations = graph.Values.Where(valve => valve.Name != state.CurrentValve && !state.OpenValves.ContainsKey(valve.Name));
+        var destinations = graph.Values.Where(valve => valve.Name != state.SelfValve && !state.OpenValves.ContainsKey(valve.Name));
         foreach (var dest in destinations)
         {
-            var costToOpen = shortKings[(state.CurrentValve, dest.Name)] + 1;
+            var costToOpen = shortKings[(state.SelfValve, dest.Name)] + 1;
             if (costToOpen > state.TimeRemaining) continue;
 
             ret.Add(new State
             {
-                CurrentValve = dest.Name,
+                SelfValve = dest.Name,
                 OpenValves = state.OpenValves
                     .Append(new KeyValuePair<string, int>(dest.Name, dest.FlowRate * (state.TimeRemaining - costToOpen)))
                     .ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
                 TimeRemaining = state.TimeRemaining - costToOpen,
             });
         }
+
+        return ret.ToArray();
+    }
+
+    public static State[] GetPossibleMoves2(State state, Dictionary<string, Valve> graph, Dictionary<(string, string), int> shortKings)
+    {
+        if (state.TimeRemaining == 0)
+            return new State[0];
+        if (state.OpenValves.Count() == graph.Count())
+            return new State[0];
+        var ret = new List<State>();
+
+        var isGolden =
+            state.OpenValves.TryGetValue("DD", out int pressure) && pressure == graph["DD"].FlowRate * 24 &&
+            state.OpenValves.TryGetValue("JJ", out pressure) && pressure == graph["JJ"].FlowRate * 23 &&
+            state.OpenValves.TryGetValue("BB", out pressure) && pressure == graph["BB"].FlowRate * 19 &&
+            state.OpenValves.TryGetValue("HH", out pressure) && pressure == graph["HH"].FlowRate * 19 &&
+            state.OpenValves.TryGetValue("CC", out pressure) && pressure == graph["CC"].FlowRate * 17;
+        
+        if (isGolden)
+        {
+            Console.WriteLine("asdfsafd");
+        }
+
+        var destinations = graph.Values.Where(d => !state.OpenValves.ContainsKey(d.Name)).ToArray();
+        if (state.SelfOpenRemaining == 0 && state.ElephantOpenRemaining == 0)
+        {
+            var destPairs = destinations.SelectMany(d => destinations.Select(d2 => (d, d2)));
+            foreach (var (self, el) in destPairs)
+            {
+                if (self.Name == el.Name) continue;
+                var selfOpen = shortKings[(state.SelfValve, self.Name)] + 1;
+                var elOpen = shortKings[(state.ElephantValve, el.Name)] + 1;
+                var costToOpen = Math.Min(selfOpen, elOpen);
+                if (costToOpen >= state.TimeRemaining) continue;
+
+                var openValves = state.OpenValves.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                if (selfOpen == costToOpen) openValves[self.Name] = self.FlowRate * (state.TimeRemaining - costToOpen);
+                if (elOpen  == costToOpen) openValves[el.Name] = el.FlowRate * (state.TimeRemaining - costToOpen);
+
+                ret.Add(new State
+                {
+                    SelfValve = self.Name,
+                    SelfOpenRemaining = selfOpen - costToOpen,
+                    ElephantValve = el.Name,
+                    ElephantOpenRemaining = elOpen - costToOpen,
+                    OpenValves = openValves,
+                    TimeRemaining = state.TimeRemaining - costToOpen,
+                });
+            }
+        }
+        else if (state.SelfOpenRemaining > 0)
+        {
+            var elDests = destinations.Where(d => d.Name != state.SelfValve);
+            var statesCreated = 0;
+            foreach (var dest in elDests)
+            {
+                var elOpen = shortKings[(state.ElephantValve, dest.Name)] + 1;
+                var costToOpen = Math.Min(state.SelfOpenRemaining, elOpen);
+                if (costToOpen >= state.TimeRemaining) continue;
+
+                var openValves = state.OpenValves.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                if (state.SelfOpenRemaining == costToOpen) openValves[state.SelfValve] = graph[state.SelfValve].FlowRate * (state.TimeRemaining - costToOpen);
+                if (elOpen == costToOpen) openValves[dest.Name] = dest.FlowRate * (state.TimeRemaining - costToOpen);
+
+                ret.Add(new State
+                {
+                    SelfValve = state.SelfValve,
+                    SelfOpenRemaining = state.SelfOpenRemaining - costToOpen,
+                    ElephantValve = dest.Name,
+                    ElephantOpenRemaining = elOpen - costToOpen,
+                    OpenValves = openValves,
+                    TimeRemaining = state.TimeRemaining - costToOpen,
+                });
+                statesCreated++;
+            }
+            if (statesCreated == 0)
+            {
+                ret.Add(new State
+                {
+                    SelfValve = state.SelfValve,
+                    SelfOpenRemaining = 0,
+                    ElephantValve = state.ElephantValve,
+                    ElephantOpenRemaining = state.ElephantOpenRemaining,
+                    OpenValves = state.OpenValves.Append(new KeyValuePair<string, int>(state.SelfValve, graph[state.SelfValve].FlowRate * (state.TimeRemaining - state.SelfOpenRemaining))).ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                    TimeRemaining = state.TimeRemaining - state.SelfOpenRemaining,
+                });
+            }
+        }
+        else if (state.ElephantOpenRemaining > 0)
+        {
+            var selfDests = destinations.Where(d => d.Name != state.ElephantValve);
+            var statesCreated = 0;
+            foreach (var dest in selfDests)
+            {
+                var selfOpen = shortKings[(state.SelfValve, dest.Name)] + 1;
+                var costToOpen = Math.Min(state.ElephantOpenRemaining, selfOpen);
+                if (costToOpen >= state.TimeRemaining) continue;
+
+                var openValves = state.OpenValves.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                if (state.ElephantOpenRemaining == costToOpen) openValves[state.ElephantValve] = graph[state.ElephantValve].FlowRate * (state.TimeRemaining - costToOpen);
+                if (selfOpen == costToOpen) openValves[dest.Name] = dest.FlowRate * (state.TimeRemaining - costToOpen);
+
+                ret.Add(new State
+                {
+                    ElephantValve = state.ElephantValve,
+                    ElephantOpenRemaining = state.ElephantOpenRemaining - costToOpen,
+                    SelfValve = dest.Name,
+                    SelfOpenRemaining = selfOpen - costToOpen,
+                    OpenValves = openValves,
+                    TimeRemaining = state.TimeRemaining - costToOpen,
+                });
+                statesCreated++;
+            }
+            if (statesCreated == 0)
+            {
+                ret.Add(new State
+                {
+                    SelfValve = state.SelfValve,
+                    SelfOpenRemaining = state.SelfOpenRemaining,
+                    ElephantValve = state.ElephantValve,
+                    ElephantOpenRemaining = 0,
+                    OpenValves = state.OpenValves.Append(new KeyValuePair<string, int>(state.ElephantValve, graph[state.ElephantValve].FlowRate * (state.TimeRemaining - state.ElephantOpenRemaining))).ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
+                    TimeRemaining = state.TimeRemaining - state.ElephantOpenRemaining,
+                });
+            }
+        }
+        else if (state.ElephantOpenRemaining > 0 && state.SelfOpenRemaining > 0)
+        {
+            Console.Write("what");
+        }
+        else
+        {
+            Console.WriteLine("what");
+        }
+
+        // foreach (var c in ret) c.Parent = state;
 
         return ret.ToArray();
     }
@@ -138,7 +316,16 @@ public record struct Valve(
 
 public class State
 {
-    public string CurrentValve { get; set; } = "";
+    public State? Parent { get; set; }
+
+    public string SelfValve { get; set; } = "";
+
+    public int SelfOpenRemaining { get; set; }
+
+    public string ElephantValve { get; set; } = "";
+
+    public int ElephantOpenRemaining { get; set; }
+
     public Dictionary<string, int> OpenValves { get; set; } = new();
     public int TimeRemaining { get; set; }
 
@@ -146,8 +333,8 @@ public class State
 
     public override int GetHashCode()
     {
-        var valveStr = string.Join("", OpenValves.Select(kvp => (kvp.Key, kvp.Value)).OrderBy(x => x));
-        var fullstr = CurrentValve + valveStr + TimeRemaining;
+        var valveStr = string.Join(":", OpenValves.Select(kvp => (kvp.Key, kvp.Value)).OrderBy(x => x));
+        var fullstr = string.Join("|", new[] { SelfValve, SelfOpenRemaining.ToString(), ElephantValve, ElephantOpenRemaining.ToString(), valveStr, TimeRemaining.ToString() });
         return fullstr.GetHashCode();
     }
 
@@ -169,8 +356,11 @@ public class State
 
         ArgumentNullException.ThrowIfNull(left);
         ArgumentNullException.ThrowIfNull(right);
-        if (left.CurrentValve != right.CurrentValve) return false;
+        if (left.SelfValve != right.SelfValve) return false;
         if (left.TimeRemaining != right.TimeRemaining) return false;
+        if (left.ElephantValve != right.ElephantValve) return false;
+        if (left.SelfOpenRemaining != right.SelfOpenRemaining) return false;
+        if (left.ElephantOpenRemaining != right.SelfOpenRemaining) return false;
 
         return left.OpenValves
             .Select(kvp => (kvp.Key, kvp.Value))
